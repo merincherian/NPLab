a3fb7dc166dc4781e26522a0c15a6638c344d38c
miniaishwarya <miniash11@gmail.com>
2019-04-05 11:17:26 +0530

    Changes to TCP

--- client_TCP.c
+++ client_TCP.c
@@ -13,24 +13,28 @@ int main(){
 
   /*---- Create the socket. The three arguments are: ----*/
   /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+  clientSocket = socket(AF_INET, SOCK_STREAM, 0);
   
   /*---- Configure settings of the server address struct ----*/
   /* Address family = Internet */
   serverAddr.sin_family = AF_INET;
+
   /* Set port number, using htons function to use proper byte order */
   serverAddr.sin_port = htons(7891);
   /* Set IP address to localhost */
+
   serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
   /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+  //memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
   /*---- Connect the socket to the server using the address struct ----*/
-  addr_size = sizeof serverAddr;
-  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+  
+  if(connect(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0 )
+    printf("There was an error in establishing connection.");
 
   /*---- Read the message from the server into the buffer ----*/
-  recv(clientSocket, buffer, 1024, 0);
+  recv(clientSocket, buffer,sizeof(buffer), 0);
 
   /*---- Print the received message ----*/
   printf("Data received: %s",buffer);   
--- server_TCP.c
+++ server_TCP.c
@@ -6,15 +6,15 @@
 #include <string.h>
 
 int main(){
-  int welcomeSocket, newSocket;
+  int server_socket, new_client_socket;
   char buffer[1024];
   struct sockaddr_in serverAddr;
-  struct sockaddr_storage serverStorage;
-  socklen_t addr_size;
+  //struct sockaddr_storage serverStorage;
+  //socklen_t addr_size;
 
   /*---- Create the socket. The three arguments are: ----*/
   /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+  server_socket = socket(AF_INET, SOCK_STREAM, 0);
   
   /*---- Configure settings of the server address struct ----*/
   /* Address family = Internet */
@@ -22,26 +22,30 @@ int main(){
   /* Set port number, using htons function to use proper byte order */
   serverAddr.sin_port = htons(7891);
   /* Set IP address to localhost */
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");  // INADDR_ANY is a constant that sets the IP address to 0.0.0.0
+ 
   /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
-
+  // memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+ 
   /*---- Bind the address struct to the socket ----*/
-  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+  bind(server_socket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
   /*---- Listen on the socket, with 5 max connection requests queued ----*/
-  if(listen(welcomeSocket,5)==0)
+  if(listen(server_socket,5)==0)
     printf("Listening\n");
   else
     printf("Error\n");
 
   /*---- Accept call creates a new socket for the incoming connection ----*/
-  addr_size = sizeof serverStorage;
-  newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+  //addr_size = sizeof serverStorage;
+  /*---- To get the address of the client connection. It is optional. ----*/
+  //new_client_socket = accept(server_socket, (struct sockaddr *) &serverStorage, &addr_size);
+  
+  new_client_socket = accept(server_socket,NULL,NULL);
 
   /*---- Send message to the socket of the incoming connection ----*/
-  strcpy(buffer,"Hello World\n");
-  send(newSocket,buffer,13,0);
+  strcpy(buffer,"You have received a message from the server. ");
+  send(new_client_socket,buffer,sizeof(buffer),0);
 
   return 0;
 }
